10fdae20e5d6047d30570bdd10d57d4e36dbdcb1
Ryan Saena <100651778+rsae920@users.noreply.github.com>
2025-11-26 21:37:08 +0000

    setup REST framework

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-j+ji%u1chj6x6ehql^mhtx6fs5x591tuihdqq$=6eweoc9_f-#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
@@ -86,10 +90,7 @@ DATABASES = {
         'CLIENT': {
             'host': 'localhost',
             'port': 27017,
-            'username': '',
-            'password': '',
             'authSource': 'admin',
-            'authMechanism': '',
         },
     }
 }
